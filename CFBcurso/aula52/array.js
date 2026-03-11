b142eae508fa2aebc40ca73adadc8415f17c612b
Rodrigo <rodrigo.tins.a@hotmail.com>
2026-03-11 16:27:24 -0300

    adicionando aula 52

--- CFBcurso/aula52/array.js
+++ CFBcurso/aula52/array.js
@@ -9,4 +9,23 @@ Retorna um iterador, para facilitar a iteração dos elementos de uma coleção,
 ->  Sets
 
 */
+const caixa = document.getElementById('caixa')
 
+let cores = ["Azul", "Verde", "Vermelho"]
+let cursos = ['JavaScript', 'Python', 'Java', cores]
+
+// cursos[0] = 'PHP'   // Altera o valor do primeiro elemento do array para 'PHP'
+
+cursos.push('HTML5')   // Adiciona um novo elemento ao FINAL do array
+cursos.unshift('CSS3')  // Adiciona um novo elemento ao INÍCIO do array
+cursos.shift()          // Remove o PRIMEIRO elemento do array
+cursos.pop()             // Remove o ÚLTIMO elemento do array
+
+console.log(cursos[3][1])   // Acessa o segundo elemento do array 'cores' que está dentro do array 'cursos', ou seja, acessa o elemento 'Verde'
+
+cursos.map((el) => {
+    let p = document.createElement("p")
+    p.innerHTML = el
+    caixa.appendChild(p)
+})
+// console.log(cursos) 
