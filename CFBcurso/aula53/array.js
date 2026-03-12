fafbad6f471cf155a998afa21f99f09a9ba6955c
Rodrigo <rodrigo.tins.a@hotmail.com>
2026-03-12 14:49:48 -0300

    Adicionando aulas 53 - 54

---
+++ CFBcurso/aula53/array.js
@@ -0,0 +1,54 @@
+/*      
+            Coleções Iteráveis    
+
+Retorna um iterador, para facilitar a iteração dos elementos de uma coleção, ou seja, ele retorna um objeto que tem a função next() que retorna o próximo elemento da coleção, e quando não houver mais elementos para iterar, ele retorna um objeto com a propriedade done: true.
+
+->      -- ARRAY --    
+->  Map
+->  Sets
+->  String
+
+*/
+const resposta = document.querySelector("#resposta");
+const button = document.getElementsByTagNameNS("button", "*")
+
+
+const op = [
+    (val) => {
+        let res = val[0]    // Começa com o primeiro número
+        for (let i = 1; i < val.length; i++) {
+            res+=val[i]   // Soma o próximo número ao resultado
+        }
+        return res;
+    },
+    (val) => {
+        let res = val[0]
+        for (let i = 1 ; i < val.length; i++) {
+            res-=val[i] // Subtrai o próximo número ao resultado
+        }
+        return res;
+    },
+    (val) => {
+        let res = val[0]
+        for (let i = 1; i < val.length; i++) {
+            res*=val[i] // Multiplica o próximo número ao resultado
+        }
+        return res;
+    },
+    (val) => {
+        let res = val[0]
+        for (let i = 1; i < val.length; i++) {
+            res/=val[i] // Divide o próximo número ao resultado
+        }
+        return res;
+    }
+]
+function Calcular(indexOperacao) {  // indexOperacao é o índice da operação selecionada, ou seja, 0 para soma, 1 para subtração, 2 para multiplicação e 3 para divisão
+    const input1 = parseFloat(document.querySelector("#input1").value);
+    const input2 = parseFloat(document.querySelector("#input2").value);
+    const valores = [input1, input2]
+
+    const res = op[indexOperacao](valores)
+    resposta.value = `O resultado é ${res}`
+    console.log(res)
+}
---
+++ CFBcurso/aula54/map.js
@@ -0,0 +1,42 @@
+/*      
+            Coleções Iteráveis    
+
+Retorna um iterador, para facilitar a iteração dos elementos de uma coleção, ou seja, ele retorna um objeto que tem a função next() que retorna o próximo elemento da coleção, e quando não houver mais elementos para iterar, ele retorna um objeto com a propriedade done: true.
+
+-> Array     
+->      -- MAP --
+->  Sets
+->  String
+
+*/
+
+const caixa = document.querySelector("#caixa");
+
+let mapa = new Map();   // Cria um novo mapa
+mapa.set('curso', 'JavaScript')
+mapa.set(10, 'CFB Cursos')
+mapa.set(1, 100)
+mapa.set('canal', 100)
+
+mapa.delete(1)   // Remove o elemento do mapa onde a chave é 1, ou seja, remove o elemento com o valor 100
+
+console.log(mapa)
+if (mapa.has('canal')){ 
+    const valor = mapa.get('canal')
+    res = `A chave 'canal' existe no mapa com o valor ${valor}` 
+
+} else {
+    res = "A chave 'canal' não existe no mapa"
+}
+
+let tamanho = mapa.size
+res += ` <br/> O tamanho da coleção é ${tamanho} `
+caixa.innerHTML = res
+
+mapa.forEach((el) => {
+    console.log(el)
+})
+// caixa.innerHTML = mapa.get('curso')
+// mapa.set('nome', 'Rodrigo')   // Adiciona um novo elemento ao mapa, onde a chave é 'nome' e o valor é 'Rodrigo'
+
+// console.log(mapa.get('nome'))   // Acessa o valor do elemento do mapa onde a chave é 'nome', ou seja, acessa o valor 'Rodrigo'
